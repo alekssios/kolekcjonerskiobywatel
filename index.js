8bdd8ca8f3e4ec8cef0b15f6f87db210c7e0132b
alekssios <102324577+alekssios@users.noreply.github.com>
2026-09-15 23:27:46 +0200

    Change image selector to '.image-upload'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 document.querySelector(".go").addEventListener('click', () => {
     var firstname = document.querySelector(".firstname");
     var surname = document.querySelector(".surname");
-    var image = document.querySelector(".image");
+    var image = document.querySelector(".image-upload");
 
     
     var borndate = document.querySelector(".borndate");
